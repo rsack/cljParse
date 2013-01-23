fdf0118904a4d71616eb225d20dcb84e4c180ec5
rsack <rsack@campus.haifa.ac.il>
2013-01-23 13:24:32 +0200

    Initial reg-ex for a basic select + function to call it

--- cljParse/src/cljParse/core.clj
+++ cljParse/src/cljParse/core.clj
@@ -1,4 +1,9 @@
 (ns cljParse.core)
 
+;; add something like parse( sqlparse( string ) )
+
+(defn chkIt? [c]
+  ( re-find #"(?i)^select .* from .*" c))
+
 (defn parse [sql-str]
-  (testit sql-str))
+  (chkIt? sql-str))
