d1153e85d18393cd4d6f6cecc8702d793a859703
rsack <rsack@campus.haifa.ac.il>
2013-01-24 21:40:30 +0200

    Comments cleanup

--- cljParse/src/cljParse/core.clj
+++ cljParse/src/cljParse/core.clj
@@ -1,27 +1,16 @@
 (ns cljParse.core)
 
 ;; TODO:
-;;      Need to find a way to wrap the long reg-exs
-;;  v-	Add something like parse( sqlparse( string ) )
-;;	?	Check if "if re-find " or cond and true/false
+;;  CHECK:  Need to find a way to wrap the long reg-exs
+;;  DECIDE:	Check if "if re-find " or cond and true/false
 
 ;; Notes:
 ;;		Careful of spaces in optional regex
-;;    Maybe split the query types into parts of the cond: (re-find) true \n (re-find) true ...
 
 (defn checkendswithsemicolon? [c]
   (if (re-find #"(?i)^.+;" c)
     true
     false))
-
-;;(defn checksql? [c]
-;;  ( re-find #"(?i)(^select .* from .*( where .*)?( group by .*)?( having .*)?( order by .*)?)|(^delete from .*( where .*)?( limit .*)?)" c))
-;;
-;;(defn test-sql [token]
-;;    (cond
-;;      (checksql? token) true
-;;      :else false))
-
 
 (defn checksql? [token]
     (cond
