42f08e2f607ea8b62cadafd80fd6a4a2d466c694
rsack <rsack@campus.haifa.ac.il>
2013-01-25 20:00:24 +0200

    Updating project definition

--- cljParse/project.clj
+++ cljParse/project.clj
@@ -1,6 +1,5 @@
-(defproject cljParse "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cljParse "0.1a"
+  :description "Clojure parser for several types of syntaxes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
